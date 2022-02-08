f5100a22f3989fe52e6c0a9468ce0607bfc10c11
Aleksandr Diakov <aldiakov@gmail.com>
2022-02-08 22:37:28 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.querySelector('body')
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let IntervalId = null;
 
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn(evt) {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;    
+    IntervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+}
+
+function onStopBtn(evt) {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(IntervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
