58ea2ca1ead767e979fa2ee1ae57d5ec97c0e1d4
Aleksandr Diakov <aldiakov@gmail.com>
2022-02-15 14:55:03 +0200

    task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('form');
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', onFormInput)
+
+let formData = {};
+let delay = 0;
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      }
+        else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`)
+      }
+    }, delay)
+  })
+};
+
+function onFormInput(evt) {
+  formData[evt.target.name] = Number(evt.target.value);
+  return formData;
+};
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  delay = formData.delay;
+  for (let position = 1; position <= formData.amount; position++) {
+    delay += formData.step;
+    
+    createPromise(position, delay)
+      .then(value => Notiflix.Notify.success(value))
+      .catch(error => Notiflix.Notify.failure(error));
   }
-}
+};
